ae0976e758c02a5b979efc987aa82c7929fe123d
Pablo Maribondo <pablomgvieira@gmail.com>
2022-01-28 16:08:46 -0300

    feat: add shopping-cart item amount

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -1,7 +1,8 @@
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class ShoppingCartService extends Service {
-  itemList = [];
+  @tracked itemList = [];
 
   addItem(item) {
     this.itemList = [...this.itemList, item];
