225f32890f12b2315933ff1e1309ee59de36ac6f
Pablo Maribondo <pablomgvieira@gmail.com>
2022-01-29 18:27:55 -0300

    feat: update item count

--- app/components/general-container.js
+++ app/components/general-container.js
@@ -3,4 +3,11 @@ import { inject as service } from '@ember/service';
 
 export default class GeneralContainerComponent extends Component {
   @service shoppingCart;
+
+  get itemCount() {
+    return this.shoppingCart.itemList.reduce((total, item) => {
+      total += item.count;
+      return total;
+    }, 0);
+  }
 }
