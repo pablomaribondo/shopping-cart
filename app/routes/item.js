ab9f0887629623ac4a97354f099caa1e1ee8733c
Pablo Maribondo <pablomgvieira@gmail.com>
2022-01-30 10:15:41 -0300

    feat: use ember data store

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,11 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ItemRoute extends Route {
+  @service store;
+
   async model(params) {
     const { item_id } = params;
 
-    const response = await fetch('/api/items.json');
-    const { data } = await response.json();
+    const data = await this.store.findAll('product');
 
     return data.find(({ id }) => id === item_id);
   }
