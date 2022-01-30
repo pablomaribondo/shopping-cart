1ee7fdc8d9d3a693249ccaad1670cad5b77b92ee
Pablo Maribondo <pablomgvieira@gmail.com>
2022-01-30 10:22:07 -0300

    feat: create application adapter & serializer

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service store;
+
   async model() {
-    const response = await fetch('/api/items.json');
-    const { data } = await response.json();
-    return data;
+    return this.store.findAll('product');
   }
 }
