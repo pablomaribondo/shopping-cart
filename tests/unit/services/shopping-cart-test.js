782daad0bed7cf4d04fa037f0aa4b670ab285541
Pablo Maribondo <pablomgvieira@gmail.com>
2022-01-31 14:42:47 -0300

    test: ensure addItem works

--- tests/unit/services/shopping-cart-test.js
+++ tests/unit/services/shopping-cart-test.js
@@ -4,9 +4,16 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Service | shopping-cart', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
-    let service = this.owner.lookup('service:shopping-cart');
+    const service = this.owner.lookup('service:shopping-cart');
     assert.ok(service);
+  });
+
+  test('addItem works', function (assert) {
+    const service = this.owner.lookup('service:shopping-cart');
+
+    assert.equal(service.itemList.length, 0, 'item list is empty');
+    service.addItem({});
+    assert.equal(service.itemList.length, 1, 'item list size increase by 1');
   });
 });
